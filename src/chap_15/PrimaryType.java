d065263fece40c5fd03e7479caa0ff43d0138bd3
doublekm153 <doublekm153@gmail.com>
2023-10-27 10:01:46 +0900

    chap_15 작성 기본 타입 선언

---
+++ src/chap_15/PrimaryType.java
@@ -0,0 +1,16 @@
+package chap_15;
+
+public class PrimaryType {
+    /*
+    * 기본 타입 선언하고 출력
+    * */
+
+    public static void main(String[] args) {
+        boolean isL = false;
+        char suit = 'H';
+        int year = 2016;
+        double latitude = 37.52127220511242;
+        double longitude = 127.0074462890625;
+        System.out.println(latitude + "\t" + longitude);
+    }
+}
