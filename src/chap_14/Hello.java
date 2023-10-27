0159bf6329dd5ae2e66d45cc3a13e3384d69c0e4
doublekm153 <doublekm153@gmail.com>
2023-10-27 09:57:39 +0900

    chap_14 작성 자바 프로그래밍 실행. "이 프로그래밍은 JAVA200이 만들었습니다." 를 콘솔에 출력 System.out.println() 이용

---
+++ src/chap_14/Hello.java
@@ -0,0 +1,12 @@
+package chap_14;
+
+public class Hello {
+    /*
+    * "이 프로그래밍은 JAVA200이 만들었습니다." 를 콘솔에 출력
+    * System.out.println() 사용
+    */
+
+    public static void main(String[] args) {
+        System.out.println("이 프로그래밍은 JAVA200이 만들었습니다.");
+    }
+}
