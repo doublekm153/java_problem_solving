200944e0672e6ae857edf2d06bda0c6008085262
doublekm153 <doublekm153@gmail.com>
2023-10-30 10:27:13 +0900

    chap_17 주석 추가(클래스, 생성자, 변수)

--- src/chap_17/JavaUserDefinedType.java
+++ src/chap_17/JavaUserDefinedType.java
@@ -10,7 +10,11 @@ public class JavaUserDefinedType {
         double latitude = 37.52127220511242;
         double longitude = 127.0074462890625;
 
-//        사용자 정의 객체 생성
+        /* 사용자 정의 객체 생성
+        * JLocation 클래스에서 new JLocation 을 통해
+        * JLocation 의 이름을 가진 생성자 생성.
+        * jloc == JLocation 클래스에서 JLocation 이름의 객체를 호출함으로 인해 사용가능한 변수 이름.
+        */
         JLocation jloc = new JLocation();
         jloc.lat= latitude;    //jloc.lat = 37.52127220511242;
         jloc.lng= longitude;    //jloc.lng = 127.0074462890625;
